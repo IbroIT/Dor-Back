7cbc7ceb8ce5f60242240b41c874eeec6534d39a
Ibro <ibroabdraimov20@gmail.com>
2025-07-12 21:05:35 +0600

    a

--- dordoi_backend/urls.py
+++ dordoi_backend/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/news/', include('news.urls')),
     path('api/press-releases/', include('press.urls')),
-    path('api/news/', include('archive.urls')),
+    path('api/archive/', include('archive.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
